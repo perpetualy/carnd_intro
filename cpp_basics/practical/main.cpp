8d84eaf1b88851dc74bdba748709e20dff023b98
flynnyao <perpetualy@outlook.com>
2017-11-10 22:10:08 +0800

    more demo.

--- cpp_basics/practical/main.cpp
+++ cpp_basics/practical/main.cpp
@@ -1,10 +1,69 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
 #include "distance.h"
+
+using namespace std;
 
 int main() {
 
     std::cout << distance(3, 4, 5) << std::endl;  
     std::cout << distance(7.0, 2.1, 5.4) << std::endl;
+
+        // initialize string variables for reading in text file lines 
+    string line;
+    stringstream ss;
+
+    // initialize variables to hold the matrix
+    vector < vector <float> > matrix;
+    vector<float> row;
+
+    // counter for characters in a text file line
+    float i;
+
+    // read in the file
+    ifstream matrixfile ("matrix.txt");
+
+    // read in the matrix file line by line
+    // parse the file
+
+    if (matrixfile.is_open()) {
+        while (getline (matrixfile, line)) {
+
+            // parse the text line with a stringstream
+            // clear the string stream to hold the next line
+            ss.clear();
+            ss.str("");
+            ss.str(line);
+            row.clear();
+
+            // parse each line and push to the end of the row vector
+            while(ss >> i) {
+                row.push_back(i);
+
+                if (ss.peek() == ',' || ss.peek() == ' ') {
+                    ss.ignore();
+                }
+            }
+
+            // push the row to the end of the matrix
+            matrix.push_back(row);
+        }
+
+        matrixfile.close();
+
+        // print out the matrix
+        for (int row = 0; row < matrix.size(); row++) {
+            for (int column = 0; column < matrix[row].size(); column++) {
+                cout << matrix[row][column] << " " ;
+            }
+            cout << endl; 
+        }
+    }
+
+    else cout << "Unable to open file";
     
     return 0;   
 }
